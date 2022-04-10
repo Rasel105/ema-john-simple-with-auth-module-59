37164e994132a9bad4a0c9c233fe7c69f85eb2ae
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-10 19:26:28 +0600

    signout added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About/About';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
+import RecaureAuth from './components/RecaureAuth';
 import Shop from './components/Shop/Shop';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
@@ -17,7 +18,11 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
-        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/inventory' element={
+          <RecaureAuth>
+            <Inventory></Inventory>
+          </RecaureAuth>
+        }></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/signup' element={<Signup />}></Route>
